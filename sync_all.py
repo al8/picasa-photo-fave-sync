21d783619be7348e435274bab3d78698e6653131
Alan Wang <alanw@fb.com>
2020-08-28 17:37:51 -0700

    Remove host/port

--- sync_all.py
+++ sync_all.py
@@ -20,8 +20,6 @@ from sync_all_lib import (cleanup_output_path, copy_resize_rotate,
 
 def main(logger):
     params = {
-        "PI-HOST": None,  # "192.168.1.34",
-        "PI-PORT": 9999,
         "output_path": r"D:\!Dropbox.com\Dropbox (Personal)\sync_output",
         "scp_cmdline": [
             r"D:\Progs\pscp.exe",
@@ -37,9 +35,6 @@ def main(logger):
         r"D:\!Dropbox.com\Dropbox (Personal)\raspberrypi-frameserver\transfer_client\jhead.exe",
     }
     set_params(params)
-
-    HOST = params["PI-HOST"]
-    PORT = int(params["PI-PORT"])
 
     transfer_params_l = [
         transfer_params_t(
@@ -133,29 +128,6 @@ def main(logger):
         logger.info("FILES RESIZED: %d" % len(new_files))
 
     cleanup_output_path(params["output_path"], all_output_files)
-
-    if HOST is None:
-        logger.info("No HOST specified, skipping upload")
-    else:
-        # list remote files to find ones to be deleted that don't exist locally
-        local_files = set(map(lambda f: os.path.basename(
-            f).lower(), list(all_output_files)))
-        remote_files = set(remote_get_files(HOST, PORT))
-
-        # upload files that don't exist on remote
-        upload_files = local_files - remote_files
-        upload(map(lambda f: os.path.join(
-            params["output_path"], f), list(upload_files)))
-
-        # remove renote files
-        delete_files = remote_files - local_files
-        if len(delete_files):
-            logger.info("num files to be deleted remotely: %d" %
-                        len(delete_files))
-        for f in delete_files:
-            logger.debug("delete file remotely: %s" % f)
-        if delete_files:
-            remote_delete_files(HOST, PORT, list(delete_files))
 
 
 if __name__ == "__main__":
